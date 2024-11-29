a231504f8575d3624a46074de36c62f1e69c4cbd
Drilordubu <diego.gonzalezsilva@gmail.com>
2024-11-28 20:48:23 -0600

    max CDT Label

--- control.js
+++ control.js
@@ -115,3 +115,18 @@ function datosSolar(hp, distPan){
   con los hp se calcula el gabinete armado y el variador normalmente el variador es el que sigue en HP ejemplo si la bomba es de 10 el variador es de 15 y asi
   esta funcion debe retornar el tipo de panel, cantidad de paneles, precio, gabinete armado precio, */
 }
+function maxCDT(){
+const dropdown = document.getElementById('ltsSelect');
+const cdtMax = document.getElementById('cdtMax');
+if (!dropdown){ console.error("Error: Could not find dropdown element.");}
+if(!cdtMax)   { console.error("Error: Could not find cdtMax element.");}
+const maxAltValues = {
+  0.6: 'CDT Max 360mts',0.96: 'CDT Max 381mts', 1.4: 'CDT Max 356mts', 
+  2.5: 'CDT Max 422mts', 4.16: 'Altura Max 123mts', 5.33: 'Altura Max 369mts', 
+  9.33: 'Altura Max 343mts', 15: 'Altura Max 242mts', 20: 'Altura Max 182mts',
+  23.3: 'Altura Max 250mts', 30: 'Altura Max 190mts', 40: 'Altura Max 144mts',
+  53.3: 'Altura Max 119mts', 70: 'Altura Max 83mts'}
+const selectedValue = dropdown.value;
+const legendText = maxAltValues[selectedValue];
+cdtMax.textContent = `(${legendText})`;
+}
